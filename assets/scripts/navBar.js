ace3a016c9e7f075763a2ede3116cd9ec70e4ef9
Gonzalo Pingo Amaya <pingoamayagonzalo@gmail.com>
2024-10-18 01:49:11 -0500

    proyecto casi completo

---
+++ assets/scripts/navBar.js
@@ -0,0 +1,10 @@
+const button = document.querySelector("#menu-button");
+const menu = document.querySelector("#menu-list");
+const icon = document.querySelector("#icon-change");
+
+button.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+  icon.classList.contains("fa-xmark")
+    ? icon.classList.replace("fa-xmark", "fa-bars")
+    : icon.classList.replace("fa-bars", "fa-xmark");
+});
